e1c76f043aa04e119560446bc4a491b318f83567
dmitry.kalyuzhnyi <dmitry.kalyuzhnyi@gmail.com>
2014-11-13 19:01:22 +0300

    SR template

--- app/src/main/java/com/android/sravnilka/RootActivity.java
+++ app/src/main/java/com/android/sravnilka/RootActivity.java
@@ -13,11 +13,16 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 //import com.android.sravnilka.ui.adapter.DragDropAdapter;
+import com.android.sravnilka.ui.adapter.DragDropAdapter;
 import com.android.sravnilka.ui.fragments.ComparatorFragment;
 import com.android.sravnilka.ui.fragments.ItemsFactoryFragment;
 //import com.android.sravnilka.ui.listeners.DragDropItemMovedListener;
 //import com.android.sravnilka.ui.listeners.DragDropLongClickListener;
 import com.android.sravnilka.ui.fragments.ParamsFactoryFragment;
+import com.android.sravnilka.ui.fragments.ResultsFragment;
+import com.android.sravnilka.ui.fragments.SorterFragment;
+import com.android.sravnilka.ui.listeners.DragDropItemMovedListener;
+import com.android.sravnilka.ui.listeners.DragDropLongClickListener;
 import com.nhaarman.listviewanimations.ArrayAdapter;
 import com.nhaarman.listviewanimations.itemmanipulation.DynamicListView;
 import com.nhaarman.listviewanimations.itemmanipulation.dragdrop.TouchViewDraggableManager;
@@ -32,18 +37,19 @@ public class RootActivity extends Activity implements IFlowController {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_root);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        if(savedInstanceState == null){
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new ItemsFactoryFragment())
-                    .commit();
-        }
-
-//        if (savedInstanceState == null) {
+//        if(savedInstanceState == null){
 //            getFragmentManager().beginTransaction()
-//                    .add(R.id.container, new PlaceholderFragment())
+//                    .add(R.id.container, new ItemsFactoryFragment())
 //                    .commit();
 //        }
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
     /**************************************************************
@@ -61,11 +67,19 @@ public class RootActivity extends Activity implements IFlowController {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case android.R.id.home:
+                FragmentManager fm = getFragmentManager();
+                for(int i = 0; i < fm.getBackStackEntryCount(); ++i) {
+                    fm.popBackStack();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     /**************************************************************
@@ -73,55 +87,59 @@ public class RootActivity extends Activity implements IFlowController {
      **************************************************************/
     @Override
     public void onItemSetReady(Set<String> items) {
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, new ParamsFactoryFragment());
-        transaction.addToBackStack(null);
-        transaction.commit();
+        openNewFragment(new ParamsFactoryFragment());
     }
 
     @Override
     public void onParamSetReady(Set<String> params) {
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, new ComparatorFragment());
-        transaction.addToBackStack(null);
-        transaction.commit();
+        openNewFragment(new ComparatorFragment());
     }
 
     @Override
     public void onCompareActionDone(Map<String, Set<String>> data) {
-
+        openNewFragment(new SorterFragment());
     }
 
     @Override
     public void onImportanceScaleReady(Map<String, Integer> importanceScale) {
+        openNewFragment(new ResultsFragment());
+    }
 
+    /**************************************************************
+     * Helper methods
+     **************************************************************/
+    private void openNewFragment(Fragment fragment){
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-//    public static class PlaceholderFragment extends Fragment {
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fr_root, container, false);
-//
-//            //Example Drag and Drop ListView
-//            Toast.makeText(getActivity(), R.string.drag_drop,Toast.LENGTH_LONG).show();
-//            DynamicListView listView = (DynamicListView) rootView.findViewById(R.id.dynamiclistview);
-//            ArrayAdapter<String> adapter = new DragDropAdapter(getActivity());
-//            listView.enableDragAndDrop();
-//            listView.setDraggableManager(new TouchViewDraggableManager(R.id.draganddrop_touchview));
-//            listView.setOnItemMovedListener(new DragDropItemMovedListener(adapter, getActivity()));
-//            listView.setOnItemLongClickListener(new DragDropLongClickListener(listView));
-//            listView.setAdapter(adapter);
-//            return rootView;
-//        }
-//    }
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fr_root, container, false);
+
+            //Example Drag and Drop ListView
+            Toast.makeText(getActivity(), R.string.drag_drop,Toast.LENGTH_LONG).show();
+            DynamicListView listView = (DynamicListView) rootView.findViewById(R.id.dynamiclistview);
+            ArrayAdapter<String> adapter = new DragDropAdapter(getActivity());
+            listView.enableDragAndDrop();
+            listView.setDraggableManager(new TouchViewDraggableManager(R.id.draganddrop_touchview));
+            listView.setOnItemMovedListener(new DragDropItemMovedListener(adapter, getActivity()));
+            listView.setOnItemLongClickListener(new DragDropLongClickListener(listView));
+            listView.setAdapter(adapter);
+            return rootView;
+        }
+    }
 
 
 
--- app/src/main/java/com/android/sravnilka/ui/fragments/ComparatorFragment.java
+++ app/src/main/java/com/android/sravnilka/ui/fragments/ComparatorFragment.java
@@ -1,9 +1,14 @@
 package com.android.sravnilka.ui.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 
 /**
  * Created by dka on 12.11.2014.
  */
 public class ComparatorFragment extends Fragment {
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
 }
